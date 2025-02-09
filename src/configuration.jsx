576c0254fdebfebb594e8026d770d471b383d35f
heyitssakinah <heyitssakinah@gmail.com>
2025-02-09 11:58:40 +0800

    Access

--- src/configuration.jsx
+++ src/configuration.jsx
@@ -12,13 +12,13 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBWsjelN-nK5Bf_13-0KToPjzxmRySg1hI",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  authDomain: "valentines-project-b2201.firebaseapp.com",
+  databaseURL: "https://valentines-project-b2201-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "valentines-project-b2201",
+  storageBucket: "valentines-project-b2201.firebasestorage.app",
+  messagingSenderId: "220673749238",
+  appId: "1:220673749238:web:b5b56bdbf441e452dafcd7",
+  measurementId: "G-NNPZMNVEES",
 };
 
   //iniitalise firebase and get database 
